b8eed09e62a685ca22b3ded2031b775308f17d06
hanva100 <nicolas.tran@supinternet.fr>
2019-10-30 22:17:27 +0100

    add 3 functions

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ let token = "0cb8487d4ca260206f4b572292084e8c82d5c48f"
 const https = require('https');
 
 exports.getMapBounds = functions.https.onRequest((request, response) => {
+    //req.body.ce que tu veux
     let latlng = "39.379436,116.091230,40.235643,116.784382";
     axios.get(`https://api.waqi.info/map/bounds/?token=${token}&latlng=${latlng}`)
-    // eslint-disable-next-line promise/always-return
         .then(res => {
             response.send(res.data);
         })
@@ -18,11 +18,29 @@ exports.getMapBounds = functions.https.onRequest((request, response) => {
         });
 });
 exports.getSearch = functions.https.onRequest((request, response) => {
+    //req.body.keyword
     let keyword = "bangalore";
-    axios.get(`https://api.waqi.info/search/?keyword${keyword}&=?token=${token}`)
+    axios.get(`https://api.waqi.info/search/?token=${token}&keyword=${keyword}`)
     // eslint-disable-next-line promise/always-return
         .then(res => {
             response.send(res.data);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+});
+exports.PolutionByCountry = functions.https.onRequest((request, response) => {
+    let keyword = "FR";
+
+    //req.body.country
+    axios.get(`https://waqi.info/rtdata/ranking/index.json`)
+        .then(res => {
+            let data = res.data.cities;
+            for (let i in data) {
+                if (data[i].country === keyword) {
+                    response.send(data[i].station.a)
+                }
+            }
         })
         .catch(err => {
             console.log(err);
